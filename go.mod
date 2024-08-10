d024864f71c70416a8993cc8c036757a5339326a
Bibin Thomas <bthomas@league.com>
2024-08-10 12:45:43 -0400

    Added openfga go sdk into the mix

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tomkaith13/openfga-authz-engine
 
 go 1.22.2
 
-require github.com/go-chi/chi v1.5.5
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/openfga/go-sdk v0.5.0
+)
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	openfga "github.com/openfga/go-sdk"
 )
 
 func main() {
@@ -13,8 +16,16 @@ func main() {
 	// Middleware for logging and recovery
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	_, err := openfga.NewConfiguration(openfga.Configuration{
+		ApiUrl: os.Getenv("FGA_API_URL"), // required, e.g. https://api.fga.example
+	})
+
+	if err != nil {
+		fmt.Println("error::", err)
+	}
 
 	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+
 		w.Write([]byte("World!!"))
 	})
 	http.ListenAndServe(":8888", r)
