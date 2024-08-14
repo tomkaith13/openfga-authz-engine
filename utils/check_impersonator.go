12e78cb1c80c2738e1042e13c70f7e1e0ace7e36
Bibin Thomas <bthomas@league.com>
2024-08-14 10:20:10 -0400

    Added more fga changes to support external resolvers

--- utils/check_impersonator.go
+++ utils/check_impersonator.go
@@ -40,3 +40,43 @@ func CheckImpersonator(fgaClient *openfgaClient.OpenFgaClient, modelId string, i
 	fmt.Println("check results: allowed = ", *data.CheckResponse.Allowed)
 	return nil
 }
+
+// CheckImpersonatorWithExternalResolver passes the current time in Context and also reaches out to external resolvers.
+// This is the equivalent of http_send in OPA
+func CheckImpersonatorWithExternalResolver(fgaClient *openfgaClient.OpenFgaClient, modelId string, impersonatorId string, userId string) error {
+	options := openfgaClient.ClientCheckOptions{
+		AuthorizationModelId: &modelId,
+	}
+
+	currentTime := time.Now().UTC()
+	formattedTime := currentTime.Format("2006-01-02T15:04:05Z")
+	fmt.Println("formated UTC time:", formattedTime)
+
+	body := openfgaClient.ClientCheckRequest{
+		User:     "user:" + userId,
+		Relation: "impersonator",
+		Object:   "user:" + impersonatorId,
+		Context:  &map[string]interface{}{"current_time": formattedTime, "external_check": externalResolver()},
+	}
+
+	data, err := fgaClient.Check(context.Background()).
+		Body(body).
+		Options(options).
+		Execute()
+
+	if err != nil {
+		return err
+	}
+
+	if data.CheckResponse.Allowed != nil && !*data.CheckResponse.Allowed {
+		return errors.New("Not Allowed Error")
+	}
+	fmt.Println("check results: allowed = ", *data.CheckResponse.Allowed)
+	return nil
+}
+
+func externalResolver() bool {
+	// Simulate work
+	time.Sleep(2 * time.Second)
+	return true
+}
